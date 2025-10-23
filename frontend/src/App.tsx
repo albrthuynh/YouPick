e87f8ae2f16a45fb30b41b9c84754b427ea1f674
Rogelio Lozano <rjlozano58@gmail.com>
2025-10-23 14:40:52 -0500

    changed a comment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import axios from 'axios';
 
-// PAGES IMPORTED
+// changed a comment again!
 import LandingPage from './pages/Home/LandingPage'
 import LoginPage from './pages/Auth/LoginPage'
 import SignupPage from './pages/Auth/SignupPage'
