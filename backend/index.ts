f632c272805d3f14acc14f5b8b1ab87ae7d5afab
vivilopez1 <vlope22@uic.edu>
2025-10-23 14:49:11 -0500

    rah

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,6 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
 }));
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import axios from 'axios';
 
 // Pages Imported (example comment)
+// viviana changes!
 import LandingPage from './pages/Home/LandingPage'
 import LoginPage from './pages/Auth/LoginPage'
 import SignupPage from './pages/Auth/SignupPage'
